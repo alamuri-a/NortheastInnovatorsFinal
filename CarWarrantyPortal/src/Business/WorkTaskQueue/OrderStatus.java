9cb5d0180cd32b0c6a0554cb45d8e0e38e1a5d93
NWoodward713 <woodward.ni@northeastern.edu>
2026-07-28 20:41:30 -0400

    Add custom vehicle order model and status workflow

---
+++ CarWarrantyPortal/src/Business/WorkTaskQueue/OrderStatus.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
+ */
+package Business.WorkTaskQueue;
+
+/**
+ *
+ * @author nicholaswoodward
+ */
+public enum OrderStatus {
+DRAFT,
+VALIDATED,
+SOURCING_PARTS,
+READY_FOR_PRODUCTION,
+IN_PRODUCTION,
+IN_TRANSIT,
+DELIVERED;    
+}
