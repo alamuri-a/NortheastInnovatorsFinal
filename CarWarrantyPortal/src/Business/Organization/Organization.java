339391e7bf09c3aa49f6a4a37e579d5201aeb3f0
Scorpion17471 <171501455+Scorpion17471@users.noreply.github.com>
2026-08-01 18:36:49 -0400

    removed redundant field from Organization

--- CarWarrantyPortal/src/Business/Organization/Organization.java
+++ CarWarrantyPortal/src/Business/Organization/Organization.java
@@ -24,7 +24,6 @@ public abstract class Organization {
     Enterprise company;
     WorkQueue inTasks;
     WorkQueue outTasks;
-    Role role;
     
     // CONSTRUCTORS
     public Organization(String n, int ID, Enterprise enterprise) {
